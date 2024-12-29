fe3882704d852780df2192cd084bc12d8f323cfb
DenkiTheGamer <denkithegamer@gmail.com>
2024-12-29 17:05:28 +0100

    updated nvim plugins

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,8 +6,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.diagnostics.gdlint,
-				-- null_ls.builtins.formatting.gdformat,
+				null_ls.builtins.diagnostics.gdlint,
+				null_ls.builtins.formatting.gdformat,
 				null_ls.builtins.formatting.clang_format,
 			},
 		})
